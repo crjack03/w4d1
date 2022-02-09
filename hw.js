c9ee5b5a3bbcbd10d5f7e5a8961d17405396e796
Christopher Jackson <Chris2003jackson@gmail.com>
2022-02-09 11:46:52 -0500

    push

---
+++ hw.js
@@ -0,0 +1,25 @@
+// 1
+const myArray = [];
+for (let i = 1; i <= 10; i++) {
+  myArray.push(i);
+}
+// 2
+const myArray = [];
+let i = 1;
+while( i <= 10) {
+  myArray.push(i);
+  i++;
+}
+console.log(myArray)
+//3
+const myArray = [];
+for (let i = 11; i <= 21; i++) {
+  myArray.push(i);
+}
+console.log(myArray)
+//4
+const myArray = [];
+for (let i = 15; i >= 1; i -=2 ) {
+  myArray.push(i);
+}
+console.log(myArray)
